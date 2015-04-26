fb205b64977dcffbd3a804ea784e8e9120db9102
waqar-alamgir <waqarcs@yahoo.com>
2015-04-26 23:15:13 +0500

    CLI input added

--- src/httpbuilder/httpbuilder.go
+++ src/httpbuilder/httpbuilder.go
@@ -14,24 +14,30 @@ func Request(data string, success chan string, failed chan string) {
 	arr := strings.Split(data, "|")
 	
 	// If 3 items in array
-	if len(arr) == 3 {
-		
-		url := arr[0]
-		method := arr[1]
-		params := arr[2]
+	if len(arr) == 4 {
+		jobType := arr[0]
+		url := arr[1]
+		method := arr[2]
+		params := arr[3]
 
-		// dispatch the request
-		errb, resp := urlreq.Dispatch(url + params, method, false)
+		if jobType == "URL" {
 
-		if errb == true {
-			output := url + "|" + method + "|" + params + "|" + string(resp[:])
-			logger.Log("Success url " + url + params , DEBUG_LEVEL_LONG)
-			// logger.Log("Output " + output , DEBUG_LEVEL_LONG)
-			success <- output
+			// dispatch the request
+			errb, resp := urlreq.Dispatch(url + params, method, false)
+
+			if errb == true {
+				output := url + "|" + method + "|" + params + "|" + string(resp[:])
+				logger.Log("Success url " + url + params , DEBUG_LEVEL_LONG)
+				// logger.Log("Output " + output , DEBUG_LEVEL_LONG)
+				success <- output
+			} else {logger.Log("Invalid type: " + data , DEBUG_LEVEL_LONG)
+				logger.Log("Failed url " + url + params, DEBUG_LEVEL_LONG)
+				failed <- data
+			}
 		} else {
-			logger.Log("Failed url " + url + params, DEBUG_LEVEL_LONG)
-			failed <- data
+			logger.Log("Invalid jobType: " + jobType , DEBUG_LEVEL_LONG)
 		}
+
 	} else {
 		logger.Log("Invalid input: " + data , DEBUG_LEVEL_LONG)
 	}
